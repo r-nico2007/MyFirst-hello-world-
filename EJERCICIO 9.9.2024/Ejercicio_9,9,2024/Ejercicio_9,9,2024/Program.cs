f5176e5a48d3a996bb20aa2b5dfb9d9f3695fe94
Nicolas Ramirez <nicolasramz216@gmail.com>
2024-09-09 22:50:40 -0500

    Ejercicio en clase

--- EJERCICIO 9.9.2024/Ejercicio_9,9,2024/Ejercicio_9,9,2024/Program.cs
+++ EJERCICIO 9.9.2024/Ejercicio_9,9,2024/Ejercicio_9,9,2024/Program.cs
@@ -9,7 +9,6 @@ public class Program
     private static void Main(string[] args)
     {
         double num1, num2, num3;
-        bool esmayor, esintermedio, esmenor;
 
         Console.Write("Ingrese el primer numero: ");
         num1 = Convert.ToDouble(Console.ReadLine());
@@ -33,8 +32,32 @@ public class Program
                 Console.Write("Numero 1  es el mayor, numero 3 es el intermedio y numero 2 es el menor");
             }
         }
-        else if (num2 > num3) && (num1 > num3)))
-                { 
+        else if ((num2 > num3) && (num2 > num1))
+        {
+            if ((num3 > num1))
+            {
+                Console.Write("Numero 2 es el mayor, numero 3 es el intermedio y número 1 es el menor.");
+            }
+            else
+            {
+                Console.Write("Numero 2 es el mayor, numero 1 es el intrermedio y el numero 3 es el menor");
+
+            }
+
+        }
+        else if ((num3 > num1) && (num3 > num2))
+        {
+            if ((num1 > num3))
+            {
+                Console.Write("Numero 3 es el mayor, numero 1 es el intermedio y numero 2 es el menor");
+            }
+            else
+            {
+                Console.Write("Numero 3 es el mayor, numero 2 es el intermedio y numero 1 es el menor");
+            }
+        }
+        
+                
     }
 }
 
