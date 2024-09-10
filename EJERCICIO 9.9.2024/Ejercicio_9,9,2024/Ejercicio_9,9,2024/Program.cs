ef7d42914bf25d5840f25a2e97ec7d1213cb3039
Nicolas Ramirez <nicolasramz216@gmail.com>
2024-09-09 20:25:17 -0500

    Ejercicio 9/9/24

---
+++ EJERCICIO 9.9.2024/Ejercicio_9,9,2024/Ejercicio_9,9,2024/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+    // Construir un programa que pida por pantalla 3 numeros 
+// y luego diga cual es el mayor, el del medio y el menor de los 
+//numeros ingresados 
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Console.Write("Ingrese el primer numero");
+        num1 = Convert.ToDouble(Console.ReadLine());
+    }
+}
